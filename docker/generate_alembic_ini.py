a23aab7152ebc4e3d4a6c569a63a3e6fa2b57f16
Anastasia <anastasiiya.p@gmail.com>
2024-06-29 13:39:04 +0300

    Fix env.py

--- docker/generate_alembic_ini.py
+++ docker/generate_alembic_ini.py
@@ -5,8 +5,10 @@ from configparser import ConfigParser
 
 load_dotenv()
 
+config_path = os.path.join(os.path.dirname(__file__), "alembic.ini")
+
 config = ConfigParser()
-config.read("alembic.ini")
+config.read(config_path)
 
 
 database_url = os.getenv("MARIADB_DATABASE_URL")
@@ -16,7 +18,7 @@ if not database_url:
 config.set("alembic", "sqlalchemy.url", database_url)
 
 
-with open("alembic.ini", "w") as configfile:
+with open(config_path, "w") as configfile:
     config.write(configfile)
 
 print("alembic.ini file has been updated with the DATABASE_URL")
--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-if [ ! -d "alembic" ]; then
-    alembic init alembic
-    alembic revision --autogenerate -m "initial_migration"
-fi
 
-python docker/generate_alembic_ini.py
+#if [ ! -d "alembic" ]; then
+#    alembic init alembic
+#    alembic revision --autogenerate -m "initial_migration"
+#else
+#    echo "Alembic directory already exists. Skipping initialization."
+#
+#fi
+
+#python docker/generate_alembic_ini.py
+
+#if [ ! -f "/app/alembic.ini" ]; then
+#    cp alembic.ini ./alembic.ini
+#fi
+
 alembic upgrade head
 python -m uvicorn main:app --host 0.0.0.0 --port 8000
